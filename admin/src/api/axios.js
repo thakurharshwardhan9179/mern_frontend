b5fd05d7e325c9a9915c3e39900baecdd5493759
Harshwardhan Sendhav <sendhavharshwardhan@gmail.com>
2026-02-19 13:05:15 +0530

    fix api baseURL for production

--- admin/src/api/axios.js
+++ admin/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mern-backend-1-wd9z.onrender.com/api",
+
 });
 
 API.interceptors.request.use((req) => {
